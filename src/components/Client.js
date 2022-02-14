367fc0e4a93f5669d4fede971be35982d0517b55
Baptiste Delhomme <baptdelhomme@gmail.com>
2022-02-14 22:02:19 +0100

    import

--- src/components/Client.js
+++ src/components/Client.js
@@ -5,6 +5,8 @@ import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 class Client extends React.Component {
 
